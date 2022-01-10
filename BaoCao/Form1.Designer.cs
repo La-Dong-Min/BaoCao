ff114475f39a2f3ac8eb9ee24774f7495851c407
minmiu2107 <minmiu2107@gmail.com>
2022-01-10 15:21:41 +0700

    Add project files.

---
+++ BaoCao/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace BaoCao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbMa = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbHoTen = new System.Windows.Forms.TextBox();
+            this.tbNoiSinh = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePickerNgaySinh = new System.Windows.Forms.DateTimePicker();
+            this.checkBoxGioiTinh = new System.Windows.Forms.CheckBox();
+            this.btSua = new System.Windows.Forms.Button();
+            this.btThoat = new System.Windows.Forms.Button();
+            this.btXoa = new System.Windows.Forms.Button();
+            this.btThem = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.colMa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colHoTen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNgaySinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colGioiTinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNoiSinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDonVi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã";
+            // 
+            // tbMa
+            // 
+            this.tbMa.Location = new System.Drawing.Point(64, 3);
+            this.tbMa.Name = "tbMa";
+            this.tbMa.Size = new System.Drawing.Size(110, 22);
+            this.tbMa.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Đơn vị";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(64, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(110, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(180, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Họ tên";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(180, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Nơi sinh";
+            // 
+            // tbHoTen
+            // 
+            this.tbHoTen.Location = new System.Drawing.Point(236, 4);
+            this.tbHoTen.Name = "tbHoTen";
+            this.tbHoTen.Size = new System.Drawing.Size(176, 22);
+            this.tbHoTen.TabIndex = 6;
+            // 
+            // tbNoiSinh
+            // 
+            this.tbNoiSinh.Location = new System.Drawing.Point(236, 31);
+            this.tbNoiSinh.Name = "tbNoiSinh";
+            this.tbNoiSinh.Size = new System.Drawing.Size(502, 22);
+            this.tbNoiSinh.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(418, 7);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Ngày sinh";
+            // 
+            // dateTimePickerNgaySinh
+            // 
+            this.dateTimePickerNgaySinh.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerNgaySinh.Location = new System.Drawing.Point(495, 3);
+            this.dateTimePickerNgaySinh.Name = "dateTimePickerNgaySinh";
+            this.dateTimePickerNgaySinh.Size = new System.Drawing.Size(118, 22);
+            this.dateTimePickerNgaySinh.TabIndex = 9;
+            // 
+            // checkBoxGioiTinh
+            // 
+            this.checkBoxGioiTinh.AutoSize = true;
+            this.checkBoxGioiTinh.Location = new System.Drawing.Point(619, 4);
+            this.checkBoxGioiTinh.Name = "checkBoxGioiTinh";
+            this.checkBoxGioiTinh.Size = new System.Drawing.Size(119, 21);
+            this.checkBoxGioiTinh.TabIndex = 10;
+            this.checkBoxGioiTinh.Text = "Giới tính: Nam";
+            this.checkBoxGioiTinh.UseVisualStyleBackColor = true;
+            // 
+            // btSua
+            // 
+            this.btSua.Location = new System.Drawing.Point(500, 432);
+            this.btSua.Name = "btSua";
+            this.btSua.Size = new System.Drawing.Size(116, 37);
+            this.btSua.TabIndex = 11;
+            this.btSua.Text = "Sửa";
+            this.btSua.UseVisualStyleBackColor = true;
+            // 
+            // btThoat
+            // 
+            this.btThoat.Location = new System.Drawing.Point(622, 432);
+            this.btThoat.Name = "btThoat";
+            this.btThoat.Size = new System.Drawing.Size(116, 37);
+            this.btThoat.TabIndex = 12;
+            this.btThoat.Text = "Thoát";
+            this.btThoat.UseVisualStyleBackColor = true;
+            // 
+            // btXoa
+            // 
+            this.btXoa.Location = new System.Drawing.Point(378, 432);
+            this.btXoa.Name = "btXoa";
+            this.btXoa.Size = new System.Drawing.Size(116, 37);
+            this.btXoa.TabIndex = 13;
+            this.btXoa.Text = "Xóa";
+            this.btXoa.UseVisualStyleBackColor = true;
+            // 
+            // btThem
+            // 
+            this.btThem.Location = new System.Drawing.Point(256, 432);
+            this.btThem.Name = "btThem";
+            this.btThem.Size = new System.Drawing.Size(116, 37);
+            this.btThem.TabIndex = 14;
+            this.btThem.Text = "Thêm";
+            this.btThem.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colMa,
+            this.colHoTen,
+            this.colNgaySinh,
+            this.colGioiTinh,
+            this.colNoiSinh,
+            this.colDonVi});
+            this.dataGridView1.Location = new System.Drawing.Point(7, 59);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(731, 355);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // colMa
+            // 
+            this.colMa.HeaderText = "Mã";
+            this.colMa.Name = "colMa";
+            this.colMa.Width = 120;
+            // 
+            // colHoTen
+            // 
+            this.colHoTen.HeaderText = "Họ tên";
+            this.colHoTen.Name = "colHoTen";
+            this.colHoTen.Width = 160;
+            // 
+            // colNgaySinh
+            // 
+            this.colNgaySinh.HeaderText = "Ngày sinh";
+            this.colNgaySinh.Name = "colNgaySinh";
+            this.colNgaySinh.Width = 110;
+            // 
+            // colGioiTinh
+            // 
+            this.colGioiTinh.HeaderText = "Giới tính";
+            this.colGioiTinh.Name = "colGioiTinh";
+            // 
+            // colNoiSinh
+            // 
+            this.colNoiSinh.HeaderText = "Nơi sinh";
+            this.colNoiSinh.Name = "colNoiSinh";
+            this.colNoiSinh.Width = 110;
+            // 
+            // colDonVi
+            // 
+            this.colDonVi.HeaderText = "Đơn vị";
+            this.colDonVi.Name = "colDonVi";
+            this.colDonVi.Width = 120;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 476);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btThem);
+            this.Controls.Add(this.btXoa);
+            this.Controls.Add(this.btThoat);
+            this.Controls.Add(this.btSua);
+            this.Controls.Add(this.checkBoxGioiTinh);
+            this.Controls.Add(this.dateTimePickerNgaySinh);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbNoiSinh);
+            this.Controls.Add(this.tbHoTen);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbMa);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbMa;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbHoTen;
+        private System.Windows.Forms.TextBox tbNoiSinh;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePickerNgaySinh;
+        private System.Windows.Forms.CheckBox checkBoxGioiTinh;
+        private System.Windows.Forms.Button btSua;
+        private System.Windows.Forms.Button btThoat;
+        private System.Windows.Forms.Button btXoa;
+        private System.Windows.Forms.Button btThem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colMa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colHoTen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNgaySinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colGioiTinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNoiSinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDonVi;
+    }
+}
+
